3ce550529020571b620b4c063b32fc9ec1a00aab
shankar kumar <kumarshankar.1992@gmail.com>
2024-03-02 12:59:00 +0530

    Update llava_loader_cpp.sh

--- llava_loader_cpp.sh
+++ llava_loader_cpp.sh
@@ -1,1 +1,19 @@
-# Temp code to load llava using llama cpp
+# Clone llama cpp 
+git clone https://github.com/ggerganov/llama.cpp.git
+cd llama.cpp
+
+# Download the quantized GGUF Llava model
+wget https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/ggml-model-q5_k.gguf
+wget https://huggingface.co/mys/ggml_llava-v1.5-13b/resolve/main/mmproj-model-f16.gguf
+
+# use CMAKE to build
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+cd ..
+
+# Use Llava-cli, pass the model, proj file and image, you can use -p command line argument to pass the prompt, by default describe the image prompt is executed on the passed image
+build/bin/llava-cli -m ggml-model-q5_k.gguf --mmproj mmproj-model-f16.gguf --image sample_image.png
+
+
